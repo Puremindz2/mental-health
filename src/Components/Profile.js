4bada4f5acb34d022f57003ccd314fe8c01f6486
molapisi91 <69822799+molapisi91@users.noreply.github.com>
2023-10-02 14:57:56 +0200

    profile update

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,8 +1,52 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import appIcon from "./Images/mentalHealthIcon.png";
+import './Profile.css';
 
 export default function Account({ session }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  function handleClick1() {
+    window.location.href = '/';
+  }
+
+  function handleClick2() {
+    window.location.href = '/login';
+  }
+
+  function handleClick3() {
+    window.location.href = '/signup';
+  }
+  function handleClick() {
+    window.location.href = '/';
+  }
+
+  const handleMenuClick = (menuItem) => {
+    // Handle menu item click (e.g., navigate to different pages)
+    switch (menuItem) {
+      case 'profile':
+        history.push("/profile");
+        break;
+      case 'settings':
+        // Handle settings click
+        history.push("/settings");
+        break;
+      case 'appointments':
+        // Handle chat click
+        history.push("/appointments");
+        break;
+      case 'about':
+        // Handle about us click
+        history.push("/about");
+        break;
+      default:
+        break;
+    }
+  };
+
   const [loading, setLoading] = useState(true);
   const [avatarUrl, setAvatarUrl] = useState(null);
   const [userEmail,setUserEmail] = useState(null);
@@ -82,12 +126,54 @@ export default function Account({ session }) {
       setAvatarUrl(avatarUrl);
     }
     setLoading(false);
+
+    
   }
 
   return (
-    <form onSubmit={updateProfile} className="form-widget">
+    
+    
+    <div class="profile">
+      <div class="topbar-container">
+      <div class="topbar-left">
+      <img src={appIcon}/>
+        <span class="app-name" onClick={handleClick}>Pure Minds - Mental Health</span>
+
+      </div>
+
       <div>
-        <label htmlFor="useremail">User Email</label>
+        <h1>Pure Minds - Mental Health</h1>
+      </div>
+
+      <div class="topbar-right">
+      <div className="home-container">
+        <span>
+          <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+            <div className="menu-line"></div>
+            <div className="menu-line"></div>
+            <div className="menu-line"></div>
+          </div>
+        </span>
+      </div>
+    </div>
+
+    {isMenuOpen && (
+      <div className="menu-dropdown">
+        <ul>
+          <li onClick={() => handleMenuClick('profile')}>Profile</li>
+          <li onClick={() => handleMenuClick('settings')}>Settings</li>
+          <li onClick={() => handleMenuClick('appointments')}>Appointments</li>
+          <li onClick={() => handleMenuClick('about')}>About Us</li>
+        </ul>
+      </div>
+    )}
+    </div>
+    <div class="profile-form">
+    <form onSubmit={updateProfile} class="form-widget">
+    <img class="userIcon1"  src={appIcon}/>
+      <div>
+      <br></br>
+        <label htmlFor="useremail">User Email</label><br></br>
         <input
           id="useremail"
           type="text"
@@ -97,7 +183,7 @@ export default function Account({ session }) {
       </div>
       
       <div>
-        <label htmlFor="firstName">First Name</label>
+        <label htmlFor="firstName">First Name</label><br></br>
         <input
           id="firstName"
           type="text"
@@ -107,7 +193,7 @@ export default function Account({ session }) {
         />
       </div>
       <div>
-        <label htmlFor="lastName">Last Name</label>
+        <label htmlFor="lastName">Last Name</label><br></br>
         <input
           id="lastName"
           type="text"
@@ -117,7 +203,7 @@ export default function Account({ session }) {
         />
       </div>
       <div>
-        <label htmlFor="age">Age</label>
+        <label htmlFor="age">Age</label><br></br>
         <input
           id="age"
           type="number"
@@ -126,7 +212,7 @@ export default function Account({ session }) {
         />
       </div>
       <div>
-        <label htmlFor="gender">Gender</label>
+        <label htmlFor="gender">Gender</label><br></br>
         <input
           id="gender"
           type="text"
@@ -135,7 +221,7 @@ export default function Account({ session }) {
         />
       </div>
       <div>
-        <label htmlFor="dob">Date of Birth</label>
+        <label htmlFor="dob">Date of Birth</label><br></br>
         <input
           id="dob"
           type="date"
@@ -143,18 +229,20 @@ export default function Account({ session }) {
           onChange={(e) => setDob(e.target.value)}
         />
       </div>
-
+      <br></br>
       <div>
         <button className="button block primary" type="submit" disabled={loading}>
           {loading ? 'Loading ...' : 'Update'}
         </button>
       </div>
-
+      <br></br>
       <div>
-        <button className="button block" type="button" onClick={(e) => signOut()}>
+        <button class="button-block-primary" type="button" onClick={(e) => signOut()}>
           Sign Out
         </button>
       </div>
     </form>
+    </div>
+    </div>
   );
 }
